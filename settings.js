59fc6621e1f25935328fd7c0d35ebe3bf7c58dd9
lasitha1234 <107338685+lasitha1234@users.noreply.github.com>
2022-06-12 10:58:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94771116972']
+global.premium = ['94771116972']
+global.ownernomer = '94771116972'
+global.ownername = '🐦lasitha md'
+global.botname = '🐦lasitha md'
+global.footer = 'lasitha.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'lasitha sandaru'
+global.author = 'LSM'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
